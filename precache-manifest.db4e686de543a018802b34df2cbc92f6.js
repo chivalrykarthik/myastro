76059800743e0d19913cc5818e9dcc4388bda7cf
Karthik Arumugam <chivalrykarthik@gmail.com>
2020-09-20 23:23:02 +0530

    Updates

---
+++ precache-manifest.db4e686de543a018802b34df2cbc92f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4fdd73182deca3f5be2af0ab609a8e",
+    "url": "/myastro/index.html"
+  },
+  {
+    "revision": "f4bf904feb73c03db11b",
+    "url": "/myastro/static/css/main.b75f4ecd.chunk.css"
+  },
+  {
+    "revision": "e996d0f2aaca4963e76f",
+    "url": "/myastro/static/js/2.ca3fd9ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myastro/static/js/2.ca3fd9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4bf904feb73c03db11b",
+    "url": "/myastro/static/js/main.b10ee415.chunk.js"
+  },
+  {
+    "revision": "68651a1d01ed20be74ff",
+    "url": "/myastro/static/js/runtime-main.d17ad2c5.js"
+  }
+]);
